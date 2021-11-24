9d038cbfb6730d7658720e7d14674f047910f355
Samuel <s.contrerasmachado01@gmail.com>
2021-11-24 01:37:33 -0400

    Added color

--- jonmircha-exercises/app.js
+++ jonmircha-exercises/app.js
@@ -1,6 +1,18 @@
-
-//Detect the 'Enter' key and print the string length in "result".
-MyInput.setObj = document.getElementById("input");
-MyInput.setValue = document.getElementById("input").value;
-
-console.log(MyInput.value);
+import {Input, modifyHTMLValue} from "./classes-methods/input-output-manipulation.js";
+/* variables declaration */
+let inputIdN1 = "input-n-1";
+let resultIdN1 = "result-n-1";
+/* First exercise */
+document.getElementById(inputIdN1).addEventListener("keydown", function (event){
+    if (event.key == 'Enter') {
+        Input.setValue = document.getElementById(inputIdN1).value;
+        if(Input.isString(Input.value)){
+            document.getElementById(resultIdN1).style.color = 'black';
+            modifyHTMLValue(resultIdN1, Input.getLength);
+        }
+        else{
+            document.getElementById(resultIdN1).style.color = 'red';
+            modifyHTMLValue(resultIdN1, "Introduce una cadena de texto, por favor!");
+        }
+    }
+});
--- jonmircha-exercises/classes-methods/input-output-manipulation.js
+++ jonmircha-exercises/classes-methods/input-output-manipulation.js
@@ -1,28 +1,24 @@
-export class Input{
-    //vars declarations
-    value = undefined;
-    obj = undefined;
-    static isString() {
-      if (isNaN(this.Value)) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    static set setObj(Obj) {
-      this.Obj = Obj;
-    }
-    static set setValue(inputValue) {
-      this.Value = Value;
-    }
-    static get getLength() {
-      return this.input.length;
-    }
-    static printLength(inputId) {
-      document.getElementById("result").innerHTML = MyInput.getLength;
-    }
+export class Input {
+  //vars declarations
+  value = undefined;
+  obj = undefined;
+  static isString() {
+    return isNaN(this.value);
   }
+  static set setObj(Obj) {
+    this.Obj = Obj;
+  }
+  static set setValue(value) {
+    this.value = value;
+  }
+  static get getLength() {
+    return this.value.length;
+  }
+  static printLength(inputId) {
+    document.getElementById("result").innerHTML = MyInput.getLength;
+  }
+}
 
-    export function modifyHTMLValue(objId, value){
-        document.getElementById(objId).innerHTML = value;
-    }
+export function modifyHTMLValue(objId, value) {
+  document.getElementById(objId).innerHTML = value;
+}
